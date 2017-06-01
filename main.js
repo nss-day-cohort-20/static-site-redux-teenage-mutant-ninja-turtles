46082cb187f40ae43a90911d73a3e322ad00518f
Dustin Walker <thewalkman1234@gmail.com>
2017-06-01 12:00:07 -0500

    product page and js work

--- main.js
+++ main.js
@@ -3,55 +3,55 @@
 var products = [
 	{
 		name: "Big foot"
-		description:
+		description:"Big Foot subs are for the hungriest of hungry people. These subs are packed with delicious meat."
 		price:"$500"
 		imgUrl:
 	},
 	{
 		name: "Dragon"
-		description:
+		description:"Mythical Meats brand canned dragon meat is one of the best-tasting, and only dragon meat out there!"
 		price:"$600"
 		imgUrl:
 	},
 	{
 		name: "Unicorn"
-		description:
+		description:"Mythical Meats brand canned unicorn meat packs magic, rainbows, and awesome in every bite."
 		price:"$450"
 		imgUrl:
 	},
 	{
 		name: "Troll" 
-		description:
+		description:"Troll, it's not great... but it's mythical"
 		price:"$150"
 		imgUrl:
 	},
 	{
 		name: "Goblin" 
-		description:
+		description:"Packaged goblin meat is great for a quick bite anytime!"
 		price:"$200"
 		imgUrl:
 	},
 	{
 		name: "Centaur" 
-		description:
+		description:"Oven roasted faun legs, served with roasted red potatoes."
 		price:"$900"
 		imgUrl:
 	},
 	{
 		name: "Griffin"
-		description:
+		description:"Grilled griffon steaks are seasoned with italian herbs and spices."
 		price:"$500"
 		imgUrl:
 	},
 	{
 		name: "Cthulhu"
-		description:
+		description:"Oven roasted cthulhu, served with tangy marinara and ranch for dunking."
 		price:"$1,000"
 		imgUrl:
 	},
 	{
 		name: "Phoenix"
-		description:
+		description:"Phoenix is served raw to allow for its natural internal flame to cook itself right before your eyes. Once you finish eating, a new phoenix rises from the ashes on your plate."
 		price:"$1,500"
 		imgUrl:
 	}
@@ -89,14 +89,4 @@ for ( var loopTracker = 0; loopTracker < products.length; loopTracker += 1) {
 }
 
 
-for (var i = 0; i < img.length; i++) {
-    for (var k = 0; k < name.length; k++) {  
-    	for (var t = 0; t < description.length; t++) {
-    		for (var n = 0; n < price.length; n++) {}
-    	}
-
-var img = [];
-var name = ["Big Foot","Dragon","Unicorn","Troll","Goblin","Centaur","Griffin","Cthulu","Phoenix"];
-var description = [];
-var price = ["$500","$600","$450","$150","$200","$900","$500","$1000","$1500"];
 
